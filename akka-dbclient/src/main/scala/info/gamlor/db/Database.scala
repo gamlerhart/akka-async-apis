ad6fb597fbd0c0d5f157868f4514adaebafc5050
Roman Stoffel <roman.stoffel@gmail.com>
2012-03-29 18:32:37 +0200

    error reporting

--- akka-dbclient/src/main/scala/info/gamlor/db/Database.scala
+++ akka-dbclient/src/main/scala/info/gamlor/db/Database.scala
@@ -18,7 +18,7 @@ object Database
     val config = DatabaseSettings(system.settings.config)
     val connectionManager =
       ConnectionManagerProvider.createConnectionManager(config.url, config.userName, config.passWord)
-    val client = new DatabaseAccess(connectionManager,system.dispatcher)
+    val client = new DatabaseAccess(connectionManager, system.dispatcher)
     client
   }
 
@@ -26,11 +26,28 @@ object Database
 
 class DatabaseAccess(val connectionManager: ConnectionManager,
                      private implicit val context: ExecutionContext) extends Extension {
+
+
+  /**
+   * Connects to the database and returns the connection in a future
+   *
+   * In case of a failure the closure finishes with a [[org.adbcj.DbException]]
+   * @return future which completes with the connection or a [[org.adbcj.DbException]]
+   */
   def connect(): Future[DBConnection] = {
-    val akkaPromise = Promise[DBConnection]
-    connectionManager.connect().addListener(new DbListener[Connection] {
-      def onCompletion(future: DbFuture[Connection]) {
-        akkaPromise.success(DBConnection(future.get()))
+    completeWithAkkaFuture[Connection,DBConnection](() => connectionManager.connect(), c => DBConnection(c))
+  }
+
+  private def completeWithAkkaFuture[TOrignalData,TResult]
+  (futureProducingOperation: () => DbFuture[TOrignalData], transformation: TOrignalData => TResult) = {
+    val akkaPromise = Promise[TResult]
+    futureProducingOperation().addListener(new DbListener[TOrignalData] {
+      def onCompletion(future: DbFuture[TOrignalData]) {
+        try {
+          akkaPromise.success(transformation(future.get()))
+        } catch {
+          case ex: Throwable => akkaPromise.failure(ex)
+        }
       }
     })
     akkaPromise
--- akka-dbclient/src/test/scala/info/gamlor/db/SimpleSelectTestCase.scala
+++ akka-dbclient/src/test/scala/info/gamlor/db/SimpleSelectTestCase.scala
@@ -2,6 +2,7 @@ package info.gamlor.db
 
 import akka.dispatch.Await
 import akka.util.duration._
+import org.adbcj.ConnectionManagerProvider
 
 /**
  * @author roman.stoffel@gamlor.info
@@ -19,6 +20,17 @@ class SimpleSelectTestCase extends SpecBaseWithH2{
 
       val result = Await.result(connection, 5 seconds)
       connection must not be(result)
+    }
+    it("fail to get connection is reported in future"){
+      val notExistingServer: String = "adbcj:jdbc:h2:tcp://not.existing.localhost:8084/~/sample"
+      val noExistingConnection = ConnectionManagerProvider.createConnectionManager(notExistingServer,"sa","")
+      val connection = new DatabaseAccess(noExistingConnection,system.dispatcher).connect()
+      connection must not be(null)
+
+
+
+      val result = Await.ready(connection, 60 seconds)
+      result.value.get.isLeft must be (true)
     }
     it("can select 1"){
       var selectedOne = for{
